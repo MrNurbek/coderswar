2313b4621ab9e3687b7a935ae06ada8c1657edd6
MrNurbek <xamrayevnurbek0@gmail.com>
2025-04-26 19:01:17 +0500

    sadad

--- core/settings.py
+++ core/settings.py
@@ -126,8 +126,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets',  # Agar `assets/` kabi frontend static bor bo'lsa
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
