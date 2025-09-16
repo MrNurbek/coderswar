df49310165feb02f761b4f6e474b8c9320231095
MrNurbek <xamrayevnurbek0@gmail.com>
2025-09-16 22:51:29 +0500

    sadad

--- api/sidequest/serializers.py
+++ api/sidequest/serializers.py
@@ -5,9 +5,18 @@ from apps.sidequest.models import GearItem, UserGear
 
 
 class GearItemSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True, required=False, allow_null=True)
+    image_url = serializers.SerializerMethodField()
     class Meta:
         model = GearItem
-        fields = ['id', 'name', 'type', 'quality']
+        fields = ['id', 'name', 'type', 'quality','image','image_url']
+
+    def get_image_url(self, obj):
+        request = self.context.get("request")
+        if obj.image:
+            url = obj.image.url  # masalan: /media/gear_images/xxx.png
+            return request.build_absolute_uri(url) if request else url
+        return None
 
 
 class UserGearSerializer(serializers.ModelSerializer):
